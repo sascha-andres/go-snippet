a4d32cf40372009b2af7617b3c83ea40936dd1e6
Sascha Andres <sascha.andres@outlook.com>
2017-03-15 14:13:53 +0100

    Added gopos

--- main.go
+++ main.go
@@ -14,8 +14,17 @@
 
 package main
 
-import "github.com/sascha-andres/go-snippet/cmd"
+import (
+	"log"
+
+	"github.com/google/gops/agent"
+	"github.com/sascha-andres/go-snippet/cmd"
+)
 
 func main() {
+	if err := agent.Listen(nil); err != nil {
+		log.Fatal(err)
+	}
+
 	cmd.Execute()
 }
